ccd5b4f385e66e85842c6ae467c8182e0991bca3
2oothpick <kobbyboateng14@gmail.com>
2022-08-25 03:57:53 +0000

    loly

---
+++ positive.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int  m(int n0, int n1, int n2)
+{
+	int *t;
+	int sum;
+
+	t = malloc(sizeof(*t) * 3);
+	t[0] = n0;
+	t[1] = n1;
+	t[2] = n2;
+	sum = t[0] + t[1] + t[2];
+	return (sum);
+}
+int main(void)
+{
+	int done;
+	done = m(98, 402, -1024);
+
+
+	printf("%d",done);
+
+	return (0);
+}
